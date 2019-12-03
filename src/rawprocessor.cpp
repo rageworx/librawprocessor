a9443f4f16350949190c6df355a249fcd4875ef3
Raphael Kim <rageworx@gmail.com>
2019-12-03 03:14:05 +0000

    version changed to 0.9.35.150

--- src/rawprocessor.cpp
+++ src/rawprocessor.cpp
@@ -68,7 +68,7 @@ using namespace std;
 #define DEF_CALC_F_BMAX     255.0f
 #define DEF_CALC_I_BMAX     255
 
-#define DEF_LIBRAWPROCESSOR_VERSION_I_ARRAY     0,9,50,145
+#define DEF_LIBRAWPROCESSOR_VERSION_I_ARRAY     0,9,53,150
 
 ////////////////////////////////////////////////////////////////////////////////
 
